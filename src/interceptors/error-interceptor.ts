c9b179bc698bcf9198ecb1bb94b5a00f2b6afc7e
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-11-26 12:34:01 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,3 +1,4 @@
+import { StorageService } from './../services/storage.service';
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
@@ -5,21 +6,37 @@ import { Observable } from 'rxjs/Rx';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req)
-        .catch((error, caught) => {
-            let errorObj = error;
-            if (errorObj.error) {
-                errorObj = errorObj.error;
-            }
-            if (!errorObj.status) {
-                errorObj = JSON.parse(errorObj);
-            }
-            console.log("Erro detectado pelo Interceptor");
-            console.log(errorObj);
-            return Observable.throw(errorObj);
-        }) as any;
-  }
+    constructor(public storage: StorageService) {
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req)
+            .catch((error, caught) => {
+                let errorObj = error;
+                if (errorObj.error) {
+                    errorObj = errorObj.error;
+                }
+                if (!errorObj.status) {
+                    errorObj = JSON.parse(errorObj);
+                }
+                console.log("Erro detectado pelo Interceptor");
+                console.log(errorObj);
+
+                switch (errorObj.status) {
+                    case 403:
+                        this.handle403;
+                        break;
+                }
+
+                return Observable.throw(errorObj);
+            }) as any;
+    }
+
+    handle403() {
+        this.storage.setLocalUser(null);
+    }
+
+
 }
 
 export const ErrorInterceptorProvider = {
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -15,7 +15,7 @@ export class ProfilePage {
   cliente: ClienteDTO;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService) {
@@ -23,15 +23,22 @@ export class ProfilePage {
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email) {
+    if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(
           response => {
             this.cliente = response;
             this.getImageIfExists();
           },
-          error => {}
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          }
         );
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
@@ -41,7 +48,7 @@ export class ProfilePage {
         response => {
           this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
         },
-        error => {}
+        error => { }
       );
   }
 
