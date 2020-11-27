acdca2b4dcf4cdd8f1faf5335189614c0848f13b
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-11-27 19:44:04 -0300

    Terminando as funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { CartItem } from './../../models/card-item';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -36,5 +37,26 @@ export class CartPage {
         },
         error => {});
     }
+  }
+  
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
   }  
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+  
 }
