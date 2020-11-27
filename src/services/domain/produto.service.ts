25391b4634f0e367afdfac8fe28fb3f84f80aa21
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-11-27 18:31:49 -0300

    Carregando a imagem dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -6,10 +6,16 @@ import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 @Injectable()
 export class ProdutoService {
 
-  constructor(public http: HttpClient) {
-  }
+    constructor(public http: HttpClient) {
+    }
 
-  findByCategoria(categoria_id : string) {
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
-  }
+    findByCategoria(categoria_id: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, { responseType: 'blob' });
+    }
+
 }
